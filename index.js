fe414193eccc55938fd019fb05569f94b8038cea
Abenezer <abenezertesfaye92gmail.com>
2023-10-29 22:48:15 +0300

    added Player module and some methods on the gameboard module

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const SIZE = 10;
 
 class Ship {
-  constructor(length) {
+  constructor(length, name) {
+    this.name = name;
     this.length = length;
     this.hits = [];
   }
 
-  hit (position) {
-    if (position < 0 || position > SIZE || this.hits.includes(position)) return false
-    this.hits.push(position);
+  hit (x, y) {
+    if (x < 0 || y < 0 || x >= SIZE || y >= SIZE || this.hits.includes([x, y])) return false
+    this.hits.push([x, y]);
+    return true
   }
 
   isSunk () {
@@ -22,6 +24,9 @@ class Gameboard {
   constructor () {
     this.board = [];
     this.missedShots = [];
+    this.hitShots = [];
+    this.ships = [];
+    this.initialize();
   }
 
   initialize (){
@@ -39,22 +44,27 @@ class Gameboard {
   placeShip (ship, row, col, isVertical) {
     if (!this.isPlacementPossible(ship, row, col, isVertical)) return false
 
+    let sh = [];
     if (isVertical) {
       for (let i = 0; i < ship.length; i++) {
-        this.board[row + i][col] = ship
+        this.board[row + i][col] = ship.name;
+        sh.push([row + i, col]);
       }
     } else {
         for (let i = 0; i < ship.length; i++) {
-          this.board[row][col + i] = ship
+          this.board[row][col + i] = ship.name;
+          sh.push([row, col + 1]);
         }
     }
 
+    this.ships.push(sh);
+    // console.log(this.ships);
     return true
   }
 
   isPlacementPossible (ship, row, col, isVertical) {
     // check if the position is within boundary
-    if (row < 0 || row > SIZE || col < 0 || col > SIZE) return false
+    if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) return false
 
     if (isVertical) {
       if (row + ship.length > SIZE) return false
@@ -75,7 +85,104 @@ class Gameboard {
 
     return true;
   }
-  
+
+  recieveAttack (x, y) {
+    if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) return false;
+
+    if (this.board[x][y] == null) {
+      this.missedShots[x][y] = true;
+      return 'Missed Shot';
+    } else {
+      this.hitShots.push([x,y]);
+      return 'Hit Shot';
+    }
+  }
+
+  isGameOver (ships) {
+    if (this.hitShots.length === 17) return true;
+
+    // for (let i = 0; i < ships.length; i++) {
+    //   if (ships[i].isSunk()) return true;
+    // }
+    for (ships of ships) {
+      if (ships.isSunk()) return true
+    }
+   
+    return false;
+    
+    }
+
+  isOutOfBoundary (x, y) {
+    // if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) return true
+    return x < 0 || y < 0 || x >= SIZE || y >= SIZE ? true : false;
+    // return false
+  }  
+
+  getEmptyField () {
+    let result = 0;
+    for (let i = 0; i < SIZE; i++) {
+      for (let j = 0; j < SIZE; j++) {
+        if (this.board[i][j] == null) {
+          result += 1
+        }
+      }
+    }
+    return result;
+  }
+
 }
 
-module.exports = { Ship, Gameboard };
+class Player {
+  constructor (name) {
+    this.name = name;
+    this.gameBoard = new Gameboard();
+    this.ships = [];
+    this.alreadyHitPositions = [];
+  }
+
+  randomAttack(gameboard) {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+
+    while (this.alreadyHitPositions.includes(x, y)) {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+    }
+    this.alreadyHitPositions.push([x, y]);
+    gameboard.recieveAttack(x, y);
+  }
+  // the input gameboard should be the other players game board
+  attack (x, y, gameboard) {
+    if (this.alreadyHit(x, y) || gameboard.isOutOfBoundary(x, y)) return false;
+
+    this.alreadyHitPositions.push([x, y]);
+    gameboard.recieveAttack(x, y);
+  }
+
+  alreadyHit(x, y) {
+    for (let i = 0; i < this.alreadyHitPositions.length; i++) {
+      if (this.alreadyHitPositions[i][0] == x && this.alreadyHitPositions[i][1] == y) {
+        return true
+      }
+    }
+    return false;
+  }
+
+}
+
+const player1 = new Player('Player 1');
+const player2 = new Player('Player 2');
+let ship1 = new Ship(3);
+player1.gameBoard.placeShip(ship1, 0, 0, true);
+player1.gameBoard.ships.push(ship1);
+player2.gameBoard.placeShip(ship1, 0, 1, true);
+player2.gameBoard.ships.push(ship1);
+
+// while(!player1.gameBoard.isGameOver() || !player2.gameBoard.isGameOver()) {
+//   let x = input('Enter X Position: ');
+//   let y = input('Enter Y Position: ');
+//   player1.attack(x, y, player2.gameBoard);
+//   player2.randomAttack(player1.gameBoard);
+// }
+
+module.exports = { Ship, Gameboard, Player };
--- index.test.js
+++ index.test.js
@@ -1,12 +1,42 @@
-import { Gameboard, Ship } from "./index";
+import { Gameboard, Ship, Player } from "./index";
+
+describe('Ship Module', () => {
+  let testShip, testBoard;
+  beforeEach(() => {
+    testShip = new Ship(3, 'ship1');
+    testBoard = new Gameboard();
+    testBoard.placeShip(testShip, 0, 0, true);
+  })
+  test('hit method 1', () => {
+    expect(testShip.hit(-1, -1)).toBe(false);
+  })
+  test('hit method 2', () => {
+    expect(testShip.hit(1, 1)).toBe(true);
+    expect(testShip.isSunk()).toBe(false);
+  })
+  test('isSunk method 1', () => {
+    expect(testShip.isSunk()).toBe(false);
+  })
+  test('isSunk method 2', () => {
+    testShip.hit(0,0)
+    testShip.hit(1,0)
+    testShip.hit(2,0)
+    expect(testShip.isSunk()).toBe(true);
+  })
+})
+
 
 describe('Gameboard Module', () => {
-  let testBoard;
-  let testShip;
+  let testBoard, ships = [];
+  let testShip1, testShip2, testShip3, testShip4, testShip5;
   beforeEach(() => {
     testBoard = new Gameboard();
-    testBoard.initialize();
-    testShip = new Ship(5);
+    testShip1 = new Ship(5, 'ship1');
+    testShip2 = new Ship(4, 'ship2');
+    testShip3 = new Ship(3, 'ship3');
+    testShip4 = new Ship(3, 'ship4');
+    testShip5 = new Ship(2, 'ship5');
+    ships.push(testShip1, testShip2, testShip3, testShip4, testShip5);
   })
 
   test('checking initialize method', () => {   
@@ -76,38 +106,127 @@ describe('Gameboard Module', () => {
 
   // test isPlacementPossible for out of boundary
   test('out of boundary 1', () => {
-    expect(testBoard.isPlacementPossible(testShip, -1, 0, true)).toBe(false);
+    expect(testBoard.isPlacementPossible(testShip1, -1, 0, true)).toBe(false);
   })
   test('out of boundary 2', () => {
-    expect(testBoard.isPlacementPossible(testShip, 0, -1, false)).toBe(false);
+    expect(testBoard.isPlacementPossible(testShip1, 0, -1, false)).toBe(false);
   })
   test('out of boundary 3', () => {
-    expect(testBoard.isPlacementPossible(testShip, 6, 2, true)).toBe(false);
+    expect(testBoard.isPlacementPossible(testShip1, 6, 2, true)).toBe(false);
   })
   test('out of boundary 4', () => {
-    expect(testBoard.isPlacementPossible(testShip, 7, 2, false)).toBe(true);
+    expect(testBoard.isPlacementPossible(testShip1, 7, 2, false)).toBe(true);
   })
 
   // test isPlacementPossible and placeship for Already taken fields
   test('check for taken fields 1', () => {
-    expect(testBoard.isPlacementPossible(testShip, 0, 0, true)).toEqual(true);
+    expect(testBoard.isPlacementPossible(testShip1, 0, 0, true)).toEqual(true);
+    
   })
   test('check for taken field 2', () => {
-    testBoard.placeShip(testShip, 0, 0, true);
-    expect(testBoard.isPlacementPossible(testShip, 0, 0, true)).toBe(false);
+    testBoard.placeShip(testShip1, 0, 0, true);
+    testBoard.placeShip(testShip2, 0, 1, true);
+    testBoard.placeShip(testShip3, 0, 2, true);
+    testBoard.placeShip(testShip4, 0, 3, true);
+    expect(testBoard.isPlacementPossible(testShip5, 0, 3, true)).toBe(false);
   })
   test('check for taken field 3', () => {
-    testBoard.placeShip(testShip, 0, 0, true);
-    expect(testBoard.isPlacementPossible(testShip, 5, 0, true)).toBe(true);
+    testBoard.placeShip(testShip1, 0, 0, true);
+    expect(testBoard.isPlacementPossible(testShip1, 5, 0, true)).toBe(true);
   })
   test('check for taken field 3', () => {
-    testBoard.placeShip(testShip, 0, 0, false);
-    expect(testBoard.isPlacementPossible(testShip, 1, 0, true)).toBe(true);
+    testBoard.placeShip(testShip1, 0, 0, false);
+    expect(testBoard.isPlacementPossible(testShip1, 1, 0, true)).toBe(true);
+    expect(testBoard.isPlacementPossible(testShip1, 0, 1, true)).toBe(false);
   })
+
+  // placeShip Method
   test('placeship 1', () => {
-    expect(testBoard.placeShip(testShip, 0, 0, true)).toBe(true);
+    expect(testBoard.placeShip(testShip1, 0, 0, true)).toBe(true);
   })
   test('placeship 2', () => {
-    expect(testBoard.placeShip(testShip, 6, 0, true)).toBe(false);
+    expect(testBoard.placeShip(testShip1, 6, 0, true)).toBe(false)
+  })
+
+  // recieveAttack Method
+  test('out of Boundary 1', () => {
+    expect(testBoard.recieveAttack(-1, 0)).toBe(false);
+  })
+  test('out of Boundary 1', () => {
+    expect(testBoard.recieveAttack(0, -1)).toBe(false);
+  })
+  test('Hit shot', () => {
+    testBoard.placeShip(testShip1, 0, 0, true);
+    testBoard.placeShip(testShip2, 0, 1, true);
+    testBoard.placeShip(testShip3, 0, 2, true);
+    testBoard.placeShip(testShip4, 0, 3, true);
+    testBoard.placeShip(testShip5, 0, 4, true);
+    expect(testBoard.recieveAttack(0, 0)).toBe('Hit Shot');
+    expect(testBoard.recieveAttack(1, 0)).toBe('Hit Shot');
+    expect(testBoard.recieveAttack(1, 1)).toBe('Hit Shot');
+    expect(testBoard.recieveAttack(4, 6)).toBe('Missed Shot');
+  })
+  test('missed shot', () => {
+    testBoard.placeShip(testShip1, 0, 0, true);
+    expect(testBoard.recieveAttack(9, 9)).toBe('Missed Shot');
+  })
+
+  // gameOver Method
+  test('checking if the game is over by making hitshots 17', () => {
+    testBoard.hitShots.length = 17;
+    expect(testBoard.isGameOver()).toBe(true);
+  })
+  test('check gameover  1', () => {
+    expect(testBoard.isGameOver(ships)).toBe(false);
+  })
+  test('check gameover 2', () => {
+    testBoard.placeShip(testShip1, 0, 0, true);
+    testBoard.placeShip(testShip2, 0, 1, true);
+    testBoard.placeShip(testShip3, 0, 2, true);
+    testBoard.placeShip(testShip4, 0, 3, true);
+    testBoard.placeShip(testShip5, 0, 4, true);
+    testShip1.hit(0, 0)
+    expect(testBoard.isGameOver(ships)).toBe(false);
+  })
+  test('check gameover 2', () => {
+    testBoard.placeShip(testShip1, 0, 0, true);
+    testBoard.placeShip(testShip2, 0, 1, true);
+    testBoard.placeShip(testShip3, 0, 2, true);
+    testBoard.placeShip(testShip4, 0, 3, true);
+    testBoard.placeShip(testShip5, 0, 4, true);
+    testShip1.hit(0, 0)
+    testShip1.hit(1, 0)
+    testShip1.hit(2, 0)
+    testShip1.hit(3, 0)
+    testShip1.hit(4, 0)
+    testShip2.hit(0, 1)
+    testShip2.hit(1, 1)
+    testShip2.hit(2, 1)
+    testShip2.hit(3, 1)
+    testShip3.hit(0, 2)
+    testShip3.hit(1, 2)
+    testShip3.hit(2, 2)
+    testShip4.hit(0, 3)
+    testShip4.hit(1, 3)
+    testShip4.hit(2, 3)
+    testShip5.hit(0, 4)
+    testShip5.hit(1, 4)
+    expect(testBoard.isGameOver(ships)).toBe(true);
+  })
+})
+
+
+describe('Player Module', () => {
+  let player1, player2;
+  beforeEach(() => {
+    player1 = new Player('Player 1');
+    player2 = new Player('Player 2');
+  })
+  test('test for intializing Player', () => {
+    expect(new Player('Abebe').name).toBe('Abebe');
+  })
+  test('test for attack', () => {
+    expect(player1.attack (-1, 0, player2.gameBoard)).toBe(false);
+    expect(player1.attack (0, 0, player2.gameBoard)).toBe();
   })
 })
